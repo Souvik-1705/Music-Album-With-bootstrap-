09831cae69f4fe54399a625466b77e5fba047d57
Souvik-1705 <souvik.techdev1705@gmail.com>
2025-04-30 11:42:54 +0530

    Added cart icon with badge to NavigationBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import {Navbar,Nav,Container} from "react-bootstrap";
+import React, { useContext } from 'react';
+import {Navbar,Nav,Container, Badge} from "react-bootstrap";
 import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import { CartContext } from '../features/CartProvider';
 
 function NavigationBar() {
+    const{cart}=useContext(CartContext);
+    const cartCount=cart.reduce((acc,item)=>acc+item.quantity,0);
   return (
     <Navbar bg='dark' variant='dark' expand="lg">
         <Container>
@@ -13,7 +17,10 @@ function NavigationBar() {
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
                     <Nav.Link as={Link} to="/store">Store</Nav.Link>
                     <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+                    <Nav.Link as={Link} to="/cart">
+                    <FaShoppingCart size={20}/>
+                    <Badge bg='success' className='ms-1'>{cartCount}</Badge>
+                    </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
